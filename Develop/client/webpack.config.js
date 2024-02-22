0c6fa2c6fc6b65c8cbb3d676c621eac5fbc14d17
Ayden Krohn <krohnayden@gmail.com>
2024-02-22 01:24:40 -0600

    final commit

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -58,7 +58,7 @@ module.exports = () => {
             options: {
               presets: ['@babel/preset-env'],
               plugins: [
-                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/plugin-transform-object-rest-spread',
                 '@babel/transform-runtime',
               ]
             }
